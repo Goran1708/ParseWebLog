c29b12f04735c903f4241ee29e80d8bd8aa28eb8
choki <gorancolovic10@gmail.com>
2019-07-27 22:26:35 +0200

    added logic to conver log file into list of viewed pages by how much they were visited

--- log_parser.rb
+++ log_parser.rb
@@ -5,7 +5,20 @@ class LogParser
   end
 
   def pages_most_views
+    pageByIpAddressMap = Hash.new
+    @file.each do |line|
+      pageAndIpAddress = line.split(" ")
+      if (pageByIpAddressMap.key?(pageAndIpAddress.first))
+        pageByIpAddressMap[pageAndIpAddress.first] << pageAndIpAddress.last
+      else
+        pageByIpAddressMap[pageAndIpAddress.first] = [pageAndIpAddress.last]
+      end
+    end
 
+    pageByIpAddressMap = pageByIpAddressMap.map do |k, v|
+      k + " " + v.size.to_s
+    end
+    pageByIpAddressMap
   end
 
   def pages_unique_views
