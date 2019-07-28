1a721f328e14745f5039301585c4ba5c7844a9b2
choki <gorancolovic10@gmail.com>
2019-07-28 12:52:10 +0200

    added test for pages unique viewes

--- spec/log_parser_test.rb
+++ spec/log_parser_test.rb
@@ -2,10 +2,20 @@ require "minitest/spec"
 require "minitest/autorun"
 require_relative "../log_parser"
 
-describe "log_parser" do
-  it "pages most views 2 pages" do
-    path = File.join(File.dirname(__FILE__), 'webserver_test.log')
-    log_parser = LogParser.new(File.open(path, "r"))
-    log_parser.pages_most_views.must_equal ["/index 4", "/about 2"]
+describe LogParser do
+  describe "#pages_most_views" do
+    it "shows 2 pages with how much they were viewed" do
+      path = File.join(File.dirname(__FILE__), 'webserver_test.log')
+      log_parser = LogParser.new(File.open(path, "r"))
+      log_parser.pages_most_views.must_equal ["/index 4", "/about 2"]
+    end
+  end
+
+  describe "#pages_unique_views" do
+    it "shows 2 pages with how much unique viewes there were" do
+      path = File.join(File.dirname(__FILE__), 'webserver_test.log')
+      log_parser = LogParser.new(File.open(path, "r"))
+      log_parser.pages_unique_views.must_equal ["/index 3", "/about 2"]
+    end
   end
 end
