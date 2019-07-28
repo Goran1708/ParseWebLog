5876efe769091532341691b57348c9e5f1450c7e
choki <gorancolovic10@gmail.com>
2019-07-28 13:27:08 +0200

    added logic to map page by ip address

--- page_by_address_mapper.rb
+++ page_by_address_mapper.rb
@@ -5,5 +5,16 @@ class PageByIpAddressMapper
   end
 
   def mapPageByIpAddress
+    pageByIpAddressMap = Hash.new
+    @file.each do |line|
+      pageAndIpAddress = line.split(" ")
+      if (pageByIpAddressMap.key?(pageAndIpAddress.first))
+        pageByIpAddressMap[pageAndIpAddress.first] << pageAndIpAddress.last
+      else
+        pageByIpAddressMap[pageAndIpAddress.first] = [pageAndIpAddress.last]
+      end
+    end
+
+    pageByIpAddressMap
   end
 end
